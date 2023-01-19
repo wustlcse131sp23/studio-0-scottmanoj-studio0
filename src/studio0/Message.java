ced994eed6df912adb857abc6d80e7dac312a2d4
savan <savan@TheVanbam.dhcp.wustl.edu>
2023-01-19 13:47:06 -0600

    hi

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,7 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("Say hi to your group mates!");
+		System.out.println("Saanya has cute earrings");
+		
 	}
 }
